ec4b9a6cf1dbed5b78b25bb34873b7caac157e3b
Sam <sampriti.shome009@gmail.com>
2024-05-10 20:18:34 +0530

    final commit by Asian dev

---
+++ CoreJava/src/Java_Inheritance/Demo_Git.java
@@ -0,0 +1,10 @@
+package Java_Inheritance;
+
+public class Demo_Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("new test login");
+	}
+
+}
